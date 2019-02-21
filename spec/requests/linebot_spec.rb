bcc0b316b46c797ebeb9657d951bb1a2adf0c7ef
chappy30313331 <46666464+chappy30313331@users.noreply.github.com>
2019-02-22 00:19:35 +0900

    Add linebot_spec

---
+++ spec/requests/linebot_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "Linebot", type: :request do
+  describe "POST /callback" do
+    before do
+      @params = {
+        destination: "xxxxxxxxxx", 
+        events: [
+          {
+            replyToken: "replyToken",
+            type: "message",
+            timestamp: 1462629479859,
+            source: {
+              type: "user",
+              userId: "U4af4980629..."
+            },
+            message: {
+              id: "325708",
+              type: "text",
+              text: "Hello, world"
+            }
+          }
+        ]
+      }
+    end
+
+    it "responds successfully" do
+      post callback_path, params: @params, as: :json
+      expect(response).to have_http_status(200)
+    end
+  end
+end
