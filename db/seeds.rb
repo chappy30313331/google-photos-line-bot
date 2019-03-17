ac2ab318448388d5e8053a505ff6be5edbc2f0fe
chappy30313331 <46666464+chappy30313331@users.noreply.github.com>
2019-03-18 00:26:05 +0900

    Close #11
    Update db/seeds.rb with sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,8 @@
-Keyword.create(name: "ネコ")
-Keyword.create(name: "ねこ")
-Keyword.create(name: "猫")
-Keyword.create(name: "にゃ")
-Keyword.create(name: "ニャ")
-Keyword.create(name: "あんず")
+# ここで指定したキーワードのいずれかがメッセージに含まれていればボットが返信する
+# Keyword.create(name: "キーワード1")
+# Keyword.create(name: "キーワード2")
 
-TextMessage.create(content: "にゃ〜")
-TextMessage.create(content: "みゃ〜")
-TextMessage.create(content: "んにゃー")
+# 画像と一緒に返信するテキストを指定する
+# 複数指定するとランダムに選ばれる
+# TextMessage.create(content: "テキストメッセージ1")
+# TextMessage.create(content: "テキストメッセージ2")
