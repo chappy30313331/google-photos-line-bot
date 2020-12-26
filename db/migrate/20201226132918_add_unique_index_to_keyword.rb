08c2d3ae3fa0dc77eb521916abccf3fedeea18a7
chappy30313331 <46666464+chappy30313331@users.noreply.github.com>
2020-12-26 23:07:07 +0900

    Add unique index to keywords

---
+++ db/migrate/20201226132918_add_unique_index_to_keyword.rb
@@ -0,0 +1,5 @@
+class AddUniqueIndexToKeyword < ActiveRecord::Migration[6.1]
+  def change
+    add_index :keywords, :name, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_132903) do
+ActiveRecord::Schema.define(version: 2020_12_26_132918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_02_26_132903) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_keywords_on_name", unique: true
   end
 
   create_table "text_messages", force: :cascade do |t|
